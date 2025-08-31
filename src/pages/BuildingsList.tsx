d7955c4deeaa749e1e8f21fcf8d9d48529b1bac1
TrondTempia <trondkulde@gmail.com>
2025-08-31 22:10:38 +0200

    Update BuildingsList.tsx

--- src/pages/BuildingsList.tsx
+++ src/pages/BuildingsList.tsx
@@ -1,4 +1,3 @@
-import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Building, Plus, Edit } from "lucide-react";
 import { supabase } from "../lib/supabaseClient";
