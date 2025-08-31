9cd4de139530bbde1baa706e840ad53e1ce407cf
TrondTempia <trondkulde@gmail.com>
2025-08-31 22:10:23 +0200

    Update BuildingDetail.tsx

--- src/pages/BuildingDetail.tsx
+++ src/pages/BuildingDetail.tsx
@@ -1,4 +1,3 @@
-import React, { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
 import { Building, ArrowLeft, GitBranch, Edit } from "lucide-react";
 import { supabase } from "../lib/supabaseClient";
